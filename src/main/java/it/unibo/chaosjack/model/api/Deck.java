66ecfe1f497829f9715eef3cd4dd906c68d6d815
Elena Lungu <elena.lungu2@studio.unibo.it>
2026-05-06 11:50:21 +0200

    style: trimmed trailing whitespaces in Deck

--- src/main/java/it/unibo/chaosjack/model/api/Deck.java
+++ src/main/java/it/unibo/chaosjack/model/api/Deck.java
@@ -6,19 +6,19 @@ import java.util.Optional;
  * Interface representing a deck of cards.
  */
 public interface Deck {
-    
+
     /**
      * Draws a card from the top of the deck.
-     * 
+     *
      * @return an Optional containing the drawn Card, or an empty Optional if the deck is empty
      */
     Optional<Card> draw();
-    
+
     /**
      * Shuffles the remaining cards in the deck.
      */
     void shuffle();
-    
+
     /**
      * @return the number of cards left in the deck.
      */
